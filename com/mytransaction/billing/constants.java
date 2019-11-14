e782c02d341627fc4841d04f6bbd6b49635be1e5
gtechgithub <36880236+gtechgithub@users.noreply.github.com>
2019-11-14 23:13:59 +0800

    Create constants.java

---
+++ com/mytransaction/billing/constants.java
@@ -0,0 +1,17 @@
+package com.mytransaction.billing.constants;
+
+public class Constants {
+
+	public static final String DELIM = ",";
+	public static final int NO_OF_FIELDS_PAYMENTS = 5;
+	public static final int NO_OF_FIELDS_REVERSAL = 6;
+	
+	public static final String ID  = "ID";
+	public static final String Date = "Date";
+	public static final String Amount  = "Amount";
+	public static final String Merchant  = "Merchant";
+	public static final String Type  = "Type";
+	public static final String Related_Transaction  = "Related Transaction";
+
+	public static final String ddmmyyydateFormat = "dd/MM/yyyy HH:mm:ss";
+}
